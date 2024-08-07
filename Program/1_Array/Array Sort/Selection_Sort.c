e8fd23765a48b199c2d836bffef89ff4405ea039
YashSolankiHub <ys6244864@gmail.com>
2024-08-07 08:45:20 +0530

    updated

--- Program/1_Array/Array Sort/Selection_Sort.c
+++ Program/1_Array/Array Sort/Selection_Sort.c
@@ -2,22 +2,22 @@
 
 void selectionSort(int arr[], int size)
 {
-    for(int i=0; i<size; i++)
+    for (int i = 0; i < size; i++)
+    {
+        int min = arr[i];
+        int min_index = i;
+        for (int j = i + 1; j < size; j++)
         {
-            int min = arr[i];
-            int min_index=i;
-            for(int j=i+1; j<size; j++)
-                {
-                    if(arr[j] < min)
-                        {
-                            min = arr[j];
-                            min_index = j;
-                        }
-                }
-            int temp = min;
-            arr[min_index] = arr[i];
-            arr[i] = temp;
+            if (arr[j] < min)
+            {
+                min = arr[j];
+                min_index = j;
+            }
         }
+        int temp = min;
+        arr[min_index] = arr[i];
+        arr[i] = temp;
+    }
     for (int i = 0; i < size; i++)
     {
         printf("%d |", arr[i]);
@@ -25,7 +25,7 @@ void selectionSort(int arr[], int size)
 }
 void main()
 {
-    int arr[] = {30,8,2,15,12,22};
+    int arr[] = {30, 8, 2, 15, 12, 22};
     int size = sizeof(arr) / sizeof(int);
     selectionSort(arr, size);
 }
--- Program/1_Array/Array Standard problem/easy/9_Print_all_Distinct_Elements_in_given_Array.c
+++ Program/1_Array/Array Standard problem/easy/9_Print_all_Distinct_Elements_in_given_Array.c
@@ -1,37 +1,42 @@
 
 #include <stdio.h>
 
-void findElementRepetition(int arr[], int size) {
+void findElementRepetition(int arr[], int size)
+{
     int isUnique;
-    for(int i=0; i<size; i++)
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        isUnique = 1;
+        int j;
+
+        for (j = 0; j < i; j++)
         {
-            isUnique=1;
-            int j;
-            for(j=0; j<i; j++)
-                {
-                    if(arr[i] == arr[j] && i != j)
-                        {
-                            isUnique=0;
-                            break;
-                        }  
-                    
-                }
-            if(isUnique)    
-                {
-                    printf("%d |",arr[i]);
-                }
+            if (arr[i] == arr[j] && i != j)
+            {
+                isUnique = 0;
+                break;
+            }
+        
         }
+        if (isUnique)
+        {
+            printf("%d |", arr[i]);
+            sum = sum + arr[i];
+        }
+    }
+    printf("\nThe sum of unique element is: %d", sum);
 }
 
-int main() {
-    int arr[] = {4,5,6,4,10,5};
+int main()
+{
+    int arr[] = {4, 5, 6, 4, 10, 5};
     int size = sizeof(arr) / sizeof(arr[0]);
 
     findElementRepetition(arr, size);
 
     return 0;
 }
-
 
 // #include<stdio.h>
 // void main()
@@ -40,7 +45,7 @@ int main() {
 //     int size = sizeof(arr)/sizeof(int);
 
 //     for(int i=0; i<size; i++)
-//         {   
+//         {
 //             int j;
 //             for( j=0; j<i; j++)
 //                 {
@@ -53,8 +58,7 @@ int main() {
 //                 {
 //                     printf("%d |", arr[i]);
 //                 }
-            
-//         }
 
+//         }
 
 // }
